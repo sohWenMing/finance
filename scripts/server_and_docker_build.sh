01d71d0ab45adcf29af206bdc2eff947b079a5fd
nindgabeet <wenming.soh@gmail.com>
2025-02-08 06:02:42 +0800

    cleanup of all files

--- scripts/server_and_docker_build.sh
+++ scripts/server_and_docker_build.sh
@@ -4,10 +4,10 @@ set -e
 echo "this is the build script starting"
 
 BUILD_DIR="/home/nindgabeet/workspace/github.com/sohWenMing/finance/builds"
-OUTPUT_FILE="$BUILD_DIR/server"
+OUTPUT_FILE="${BUILD_DIR}/server"
 GO_FILE="/home/nindgabeet/workspace/github.com/sohWenMing/finance/cmd/server/server.go"
 PROJECT_FOLDER="/home/nindgabeet/workspace/github.com/sohWenMing/finance"
 
 GOOS=linux GOARCH=amd64 go build -o "$OUTPUT_FILE" "$GO_FILE"
 
-docker build -t nindgabeet/finance_server:latest "$PROJECT_FOLDER"
+docker build -t nindgabeet/finance_server:latest "${PROJECT_FOLDER}"
